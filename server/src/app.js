d7ccc3a5d748941edfe85dcd443ab2c5db000246
Pierre Gronnier <pierre.gronnier@gmail.com>
2026-02-03 14:42:57 +0100

    fix: change CORS configuration for deploiement

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,15 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:5173",
+      "https://odin-blog-api-lcjt.vercel.app",
+      "https://odin-blog-api-bay.vercel.app",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   }),
 );
 app.use(express.json());
